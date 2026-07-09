833213ca65b834b75600f162c4eb251eb351cb4e
Ipshita0607 <patrarangadhar73@gmail.com>
2026-07-09 21:22:26 +0530

    Time: 2 ms (98.19%) | Memory: 43.2 MB (57.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-     Stack<Character> st=new Stack<>();
-     for(char ch:s.toCharArray()){
-        if(ch=='('||ch=='{'|ch=='['){
-            st.push(ch);
-        }else{
-            if(st.isEmpty()){
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
                 return false;
             }
             char top=st.peek();
             if((ch==')' && top=='(')||
             (ch=='}' && top=='{')||
             (ch==']' && top=='[')){
-                st.pop();
-            }
-            else{
+            st.pop();
+            }else{
                 return false;
             }
+            }
         }
-     }  
-     return st.isEmpty(); 
+        return st.isEmpty();
     }
-}
+}  
+//time complexity->O(n);
+//space complexity->O(n);
